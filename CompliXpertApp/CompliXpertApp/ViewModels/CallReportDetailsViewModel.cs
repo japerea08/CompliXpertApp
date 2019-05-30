5079ff98f968d3f71fd1e8d556776610b3325dc6
Joey Perea <j.perea08@gmail.com>
2019-05-30 14:43:04 -0400

    Wired in the ability to update the database from the Call Report Details Screen

--- CompliXpertApp/CompliXpertApp/ViewModels/CallReportDetailsViewModel.cs
+++ CompliXpertApp/CompliXpertApp/ViewModels/CallReportDetailsViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using CompliXpertApp.Helpers;
 using CompliXpertApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CompliXpertApp.ViewModels
 {
-    class CallReportDetailsViewModel: AbstractNotifyPropertyChanged
+    class CallReportDetailsViewModel : AbstractNotifyPropertyChanged
     {
         //attributes
         private CallReport _callReport;
@@ -20,10 +22,16 @@ namespace CompliXpertApp.ViewModels
                 Report = _report;
                 CreatedOnMobile = Report.CreatedOnMobile;
             });
+            SaveCallReportCommand = new Command(async () => await SaveCallReportAsync());
+            DeleteCallReportCommand = new Command(async () => await DeleteCallReportAsync());
+            CloseCallReportCommand = new Command(async () => await CloseCallReportAsync());
+            IsBusy = false;
         }
         #region Properties
         public ICommand SaveCallReportCommand { get; set; }
         public ICommand DeleteCallReportCommand { get; set; }
+        public ICommand CloseCallReportCommand { get; set; }
+        public bool IsBusy { get; set;}
         public CallReport Report {
             get
             {
@@ -83,7 +91,42 @@ namespace CompliXpertApp.ViewModels
         }
         #endregion
         #region Methods
-       
+        //really an update call report
+        public async Task SaveCallReportAsync()
+        {
+            using (var context = new CompliXperAppContext())
+            {
+                IsBusy = true;
+                //retrieving entity by id
+                var entity = await context.CallReport.FirstOrDefaultAsync(x => x.CallReportId == Report.CallReportId);
+                context.Entry(entity).State = EntityState.Modified;
+                entity.CallDate = Report.CallDate;
+                entity.CustomerComments = Report.CustomerComments;
+                entity.CustomerResponse = Report.CustomerResponse;
+                entity.CallDate = Report.CallDate;
+                entity.Nationality = Report.Nationality;
+                entity.OfficerComments = Report.OfficerComments;
+                entity.OtherComments = Report.OtherComments;
+                entity.Position = Report.Position;
+                entity.Purpose = Report.Purpose;
+                entity.Reason = Report.Reason;
+                entity.ReasonforAlert = Report.ReasonforAlert;
+                entity.Reference = Report.Reference;
+                entity.Status = Report.Status;
+                await context.SaveChangesAsync();
+                IsBusy = false;
+                await App.Current.MainPage.Navigation.PopAsync();
+            }
+        }
+        //remove Call Report from local DB
+        public async Task DeleteCallReportAsync()
+        {
+
+        }
+        public async Task CloseCallReportAsync()
+        {
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
         #endregion
     }
 }
