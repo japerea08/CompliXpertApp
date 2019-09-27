6b9568437cd6728a161d825d47fb98961059cb3a
Joey Perea <j.perea08@gmail.com>
2019-09-27 13:32:16 -0400

    Created a custom renderer for Android for Editor control

---
+++ CompliXpertApp/CompliXpertApp.Android/Controls/CustomEditorRenderer.cs
@@ -0,0 +1,37 @@
+﻿using CompliXpertApp.CustomViews;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.Graphics;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer (typeof(CompliXpertApp.CustomViews.CustomEditor), typeof(CompliXpertApp.Droid.Controls.CustomEditorRenderer))]
+namespace CompliXpertApp.Droid.Controls
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    public class CustomEditorRenderer : EditorRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var customEditor = (CustomEditor) Element;
+
+                if (customEditor.BorderEnabled == true)
+                {
+                    GradientDrawable gradient = new GradientDrawable();
+                    gradient.SetCornerRadius((float)customEditor.CornerRadius);
+                    gradient.SetStroke(customEditor.StrokeThickness, customEditor.StrokeColor.ToAndroid());
+                    Control.Background = gradient;
+                    //Control.Background = Resources.GetDrawable(Resource.Drawable.editorBorder);
+                }
+            }
+        }
+    }
+#pragma warning restore CS0618 // Type or member is obsolete
+}
---
+++ CompliXpertApp/CompliXpertApp/CustomViews/CustomEditor.cs
@@ -0,0 +1,66 @@
+﻿using Xamarin.Forms;
+
+namespace CompliXpertApp.CustomViews
+{
+    public class CustomEditor : Editor
+    {
+        public static readonly BindableProperty CornerRadiusProperty = 
+            BindableProperty.Create<CustomEditor, double>(p => p.CornerRadius, 0);
+
+        public static readonly BindableProperty StrokeColorProperty =
+            BindableProperty.Create<CustomEditor, Color>(p => p.StrokeColor, Color.Transparent);
+
+        public static readonly BindableProperty StrokeThicknessProperty =
+            BindableProperty.Create<CustomEditor, int>(p => p.StrokeThickness, 0);
+
+        public static readonly BindableProperty BorderEnabledProperty =
+            BindableProperty.Create<CustomEditor, bool>(p => p.BorderEnabled, false);
+
+
+        //properties
+        public bool BorderEnabled
+        {
+            get
+            {
+                return (bool) GetValue(BorderEnabledProperty);
+            }
+            set
+            {
+                SetValue(BorderEnabledProperty, value);
+            }
+        }
+        public double CornerRadius
+        {
+            get
+            {
+                return (double) base.GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                base.SetValue(CornerRadiusProperty, value);
+            }
+        }
+        public Color StrokeColor
+        {
+            get
+            {
+                return (Color) base.GetValue(StrokeColorProperty);
+            }
+            set
+            {
+                SetValue(StrokeColorProperty, value);
+            }
+        }
+        public int StrokeThickness
+        {
+            get
+            {
+                return (int) base.GetValue(StrokeThicknessProperty);
+            }
+            set
+            {
+                SetValue(StrokeThicknessProperty, value);
+            }
+        }
+    }
+}
