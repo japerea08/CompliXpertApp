366b1a916f84123f5250f268cd9b7b206bb0fc5e
Joey Perea <j.perea08@gmail.com>
2019-11-22 17:20:35 -0500

    Initialized the Responses for the call report that is passed into the CallReportDetailsViewModel from the CallReportsList

--- CompliXpertApp/CompliXpertApp/ViewModels/CallReportDetailsViewModel.cs
+++ CompliXpertApp/CompliXpertApp/ViewModels/CallReportDetailsViewModel.cs
@@ -25,6 +25,59 @@ namespace CompliXpertApp.ViewModels
         //constructor
         public CallReportDetailsViewModel()
         {
+            MessagingCenter.Subscribe<CallReportListScreenViewModel, CallReport>(this, Message.CallReportLoaded, async (sender, _report) =>
+            {
+                Report = _report;
+                CreatedOnMobile = Report.CreatedOnMobile;
+
+                //get the questions
+                using (var context = new CompliXperAppContext())
+                {
+                    ReportType = _report.CallReportType;
+
+                    List<CallReportQuestions> Questions = await (
+                        from _q in context.CallReportQuestions
+                        where _q.Type == Report.CallReportType
+                        select new CallReportQuestions
+                        {
+                            QuestionId = _q.QuestionId,
+                            QuestionHeader = _q.QuestionHeader,
+                            Status = _q.Status,
+                            Type = _q.Type
+                        }
+                    ).ToListAsync();
+                    List<QuestionandResponse> _qr = new List<QuestionandResponse>();
+                    foreach (var question in Questions)
+                    {
+                        foreach (var response in Report.Responses)
+                        {
+                            if (question.QuestionId == response.QuestionId)
+                            {
+                                //instantiate new object
+                                QuestionandResponse questionandResponse = new QuestionandResponse();
+                                questionandResponse.QuestionHeader = question.QuestionHeader;
+                                questionandResponse.Response = response.Response;
+                                questionandResponse.QuestionId = response.QuestionId;
+                                questionandResponse.ResponseId = response.ResponseId;
+                                _qr.Add(questionandResponse);
+                                break;
+                            }
+                        }
+                    }
+                    QuestionandResponses = _qr;
+                }
+                //manipulate the stack
+                List<Page> stackPages = new List<Page>();
+                foreach (Page page in App.Current.MainPage.Navigation.NavigationStack)
+                {
+                    stackPages.Add(page);
+                }
+                App.Current.MainPage.Navigation.RemovePage(App.Current.MainPage.Navigation.NavigationStack[App.Current.MainPage.Navigation.NavigationStack.Count - 2]);
+                //place the new page into stack
+                App.Current.MainPage.Navigation.InsertPageBefore(new CompliXpertAppMasterDetailPage() { Detail = new NavigationPage(new CallReportsList()) }, App.Current.MainPage.Navigation.NavigationStack[App.Current.MainPage.Navigation.NavigationStack.Count - 1]);
+                MessagingCenter.Send<CallReportDetailsViewModel, int?>(this, Message.AccountNumber, Report.AccountNumber);
+            });
+
             MessagingCenter.Subscribe<CallReportListViewModel, CallReport>(this, Message.CallReportLoaded, async (sender, _report) =>
             {
                 Report = _report;
--- CompliXpertApp/CompliXpertApp/ViewModels/CallReportListScreenViewModel.cs
+++ CompliXpertApp/CompliXpertApp/ViewModels/CallReportListScreenViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using CompliXpertApp.Helpers;
 using CompliXpertApp.Models;
+using CompliXpertApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace CompliXpertApp.ViewModels
 {
     class CallReportListScreenViewModel : AbstractNotifyPropertyChanged
     {
         private List<CallReportList> callReportGroup = new List<CallReportList>();
+        private CallReport callReportSelected;
+        private List<CallReportType> callReportTypes;
         //properties
         public List<CallReport> CallReports { get; set; }
         public List<CallReportList> CallReportGroup
@@ -23,13 +27,30 @@ namespace CompliXpertApp.ViewModels
                 OnPropertyChanged();
             }
         }
+        public CallReport CallReportSelected
+        {
+            get
+            {
+                return callReportSelected;
+            }
+            set
+            {
+                callReportSelected = value;
+                if (callReportSelected == null)
+                    return;
+                GetCallReportDetailScreenAsync();
+                OnPropertyChanged();
+            }
+        }
 
         //methods
         public void InitializeData()
         {
             using (CompliXperAppContext context = new CompliXperAppContext())
             {
-                
+                //initialize callreporttype list
+                callReportTypes = context.CallReportType.ToList();
+
                 var newList = new List<CallReportList>();
                 //get each customer                
                 foreach (Customer customer in context.Customer)
@@ -44,16 +65,32 @@ namespace CompliXpertApp.ViewModels
                                                                                       select account.AccountNumber).FirstOrDefault()
                                                                                       orderby callReport.CallDate descending
                                                     select callReport).ToList();
+                    foreach (CallReport callreport in callReports)
+                    {
+                        callreport.Responses = (from response in context.CallReportResponse
+                                                where response.CallReportId == callreport.CallReportId
+                                                select response).ToList(); 
+                    }
                     //convert the callreport type to the name
                     dummyCallReportList.AddRange(ConvertCallReportTypeToName(context, callReports));
 
                     newList.Add(dummyCallReportList);
-                }             
+                }
 
                 CallReportGroup = newList;
             }
         }
+        private async void GetCallReportDetailScreenAsync()
+        {
+            //switching back the description to type in the selectedcallreport 
+            callReportSelected.CallReportType = (from callReportType in callReportTypes
+                                                 where callReportSelected.CallReportType == callReportType.Description
+                                                 select callReportType.Type).FirstOrDefault();
 
+            await App.Current.MainPage.Navigation.PushAsync(new CompliXpertAppMasterDetailPage() { Detail = new NavigationPage(new CallReportDetailsScreen()) });
+            MessagingCenter.Send<CallReportListScreenViewModel, CallReport>(this, Message.CallReportLoaded, callReportSelected);
+            CallReportSelected = null;
+        }
         private IEnumerable<CallReport> ConvertCallReportTypeToName(CompliXperAppContext context, IEnumerable<CallReport> callReports)
         {
             foreach (CallReport callReport in callReports)
